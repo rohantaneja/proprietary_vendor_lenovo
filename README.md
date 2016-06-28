bed493077a20d70c647f0e9f1ac55c0de6b788ca
Rohan Taneja <rohantaneja63@gmail.com>
2016-06-29 00:15:30 +0530

    proprietary: add vendor blobs

---
+++ README.md
@@ -0,0 +1,17 @@
+Vendor blobs for Lenovo A7000-a
+---------------
+
+* Check device tree here:
+https://github.com/rohantaneja/android_device_lenovo_mt6752.git
+
+* Compilation
+
+        # repo init -u git://github.com/rohantaneja/android.git -b cm-13.0
+        
+        # repo sync
+        
+        # source build/envsetup.sh
+        
+        # brunch cm_mt6752-userdebug
+
+---------------
---
+++ mt6752-vendor.mk
@@ -0,0 +1,13 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/mt6752/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/mt6752/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/mt6752/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/mt6752/proprietary/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/mt6752/proprietary/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/mt6752/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/mt6752/proprietary/vendor,system/vendor)
