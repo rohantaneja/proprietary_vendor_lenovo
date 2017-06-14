21661b5e59e202ff235533ac302b0d3d4fd911b8
Rohan Taneja <rohantaneja63@gmail.com>
2017-06-15 00:03:58 +0530

    Android.mk : fix for libion_mtk shared library

--- Android.mk
+++ Android.mk
@@ -40,4 +40,13 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libion_mtk
+LOCAL_SRC_FILES_64 := proprietary/lib64/libion_mtk.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libion_mtk.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
 endif
