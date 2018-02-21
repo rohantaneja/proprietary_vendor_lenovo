d534fb6e5a3212f990482d3434081c26fb059aeb
Rohan Taneja <rohantaneja63@gmail.com>
2018-02-22 01:38:49 +0530

    aio_row: move to aio_otfp standards
    Merged over danielhk/proprietary_vendor_lenovo_aio_otfp, git merged WIP-O tree

--- aio_row/Android.mk
+++ aio_row/Android.mk
@@ -1,21 +1,35 @@
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),aio_row)
 
 ifeq ($(USE_MTK_CAMERA_WRAPPER),true)
 include $(CLEAR_VARS)
-LOCAL_MODULE := libcam2halsensor
-LOCAL_SRC_FILES_64 := proprietary/lib64/libcam2halsensor.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libcam2halsensor.so
+LOCAL_MODULE := libcam1client
+LOCAL_SRC_FILES_64 := proprietary/lib64/libcam1client.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libcam1client.so
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libcameracustom
-LOCAL_SRC_FILES_64 := proprietary/lib64/libcameracustom.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libcameracustom.so
+LOCAL_MODULE := libcam_utils
+LOCAL_SRC_FILES_64 := proprietary/lib64/libcam_utils.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libcam_utils.so
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
@@ -41,9 +55,36 @@ LOCAL_MODULE_SUFFIX := .so
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libion_mtk
-LOCAL_SRC_FILES_64 := proprietary/lib64/libion_mtk.so
-LOCAL_SRC_FILES_32 := proprietary/lib/libion_mtk.so
+LOCAL_MODULE := libbluetooth_mtk
+LOCAL_SRC_FILES_64 := proprietary/lib64/libbluetooth_mtk.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libbluetooth_mtk.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := librilmtk
+LOCAL_SRC_FILES_64 := proprietary/lib64/librilmtk.so
+LOCAL_SRC_FILES_32 := proprietary/lib/librilmtk.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mtk-ril
+LOCAL_SRC_FILES_64 := proprietary/lib64/mtk-ril.so
+LOCAL_SRC_FILES_32 := proprietary/lib/mtk-ril.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libui_ext
+LOCAL_SRC_FILES_64 := proprietary/lib64/libui_ext.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libui_ext.so
 LOCAL_MULTILIB := both
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
